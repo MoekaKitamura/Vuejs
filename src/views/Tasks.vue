a0c9945569425139956f9f7c6bf6ce23f22dfbd9
moeka kitamura <moeka.k76@gmail.com>
2022-10-29 07:10:55 +0900

    add create and destroy methods

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <h1>taskリスト</h1>
+    <el-row style="margin: 10px 0">
+      <el-col :span="10">
+        <el-input placeholder="title" v-model="title" @submit="onSubmit"></el-input>
+      </el-col>
+      <el-col :span="24">
+        <el-input placeholder="description" v-model="description" @submit="onSubmit"></el-input>
+      </el-col>
+    </el-row>
+    <el-row style="margin: 10px 0">
+      <el-button type="primary" @click="onSubmit">作成</el-button>
+    </el-row>
     <el-button type="success" @click="getTasks()">task取得</el-button>
     <el-row :gutter="12">
       <el-col :span="24"  v-for="task in tasks.data" :key="task.id">
         <el-card class="box-card" shadow="hover" style="margin: 5px 0">
           <div class="clearfix">
+            <el-button style="float: right; margin-top: -5px" @click="deleteTask(task.id)" type="success" icon="el-icon-check" circle></el-button>
             <div>{{task.title}}</div>
             <div>{{task.description}}</div>
           </div>
@@ -30,6 +42,8 @@ export default {
   name: 'Tasks',
   data () {
     return {
+      title:"",
+      description:"",
       tasks: []
     }
   },
@@ -42,6 +56,33 @@ export default {
       )
       .then((res) => {
         this.tasks = res.data
+      })
+    },
+    onSubmit(){
+      HTTP.post(URL_DATA,{
+        title: this.title,
+        description: this.description
+      })
+        .then((res) => {
+          if (res.status === 200) {
+            //登録完了
+          } else {
+            throw new Error('レスポンスエラー')
+          }
+          this.title="";
+          this.description="";
+      })
+    },
+    deleteTask(index){
+      HTTP.delete(URL_DATA + index)
+      .then((res) => {
+        if (res.status === 200) {
+          //削除完了
+        } else {
+          throw new Error('レスポンスエラー')
+        }
+        //削除後の一覧更新として記述 
+        this.getTasks();
       })
     }
   }
